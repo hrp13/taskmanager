5d233f52d790c5ace94fda54f01c4f1a8f5fe71d
hrp13 <officialharshit03@gmail.com>
2026-05-02 11:27:27 +0530

    added homecontroller

---
+++ src/main/java/com/devops/taskmanager/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.devops.taskmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Task Manager API is running 🚀";
+    }
+}
